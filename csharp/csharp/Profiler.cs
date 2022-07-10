e3422796e6501454f42cc4b6252fb86b9792135c
DESKTOP-M3EH82I\Casey <casey.haralson@gmail.com>
2022-07-09 18:38:15 -0600

    adding the function profiler

---
+++ csharp/csharp/Profiler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace csharp
+{
+    internal class Profiler
+    {
+        /// <summary>
+        /// Profiles how long it takes for a function to run and prints the elapsed milliseconds.
+        /// You can choose to run the profiler twice against the function to see the speed once the jit compiler is taken out of the profile.
+        /// </summary>
+        /// <param name="f">function to profile</param>
+        /// <param name="runTwice">whether to run the profiler twice against the function</param>
+        public static void profileFunction(Action f, string fName = "", bool runTwice = false)
+        {
+            if (runTwice) profileFunction(f, fName, false);
+
+            var sw = new Stopwatch();
+            sw.Start();
+            f();
+            sw.Stop();
+
+            Console.WriteLine($"Function call ({fName}) took {sw.Elapsed.TotalMilliseconds} ms to complete " 
+                + (runTwice ? "after jit compilation" : "with jit compilation"));
+        }
+
+
+        /// <summary>
+        /// Test function to demo the profiler.
+        /// </summary>
+        /// <param name="n">number of times to loop the incrementer</param>
+        public static void loopIncrementerTest(int n)
+        {
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                count++;
+            }
+        }
+    }
+}
--- csharp/csharp/Program.cs
+++ csharp/csharp/Program.cs
@@ -6,7 +6,23 @@ namespace csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("C# snippets!\n\n");
+
+            Console.WriteLine("Console arguments example:");
+            Console.WriteLine(args.Length + " console arguments");
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(i + ": " + args[i]);
+            }
+            Console.WriteLine("\n\n");
+
+
+
+            Profiler.profileFunction(() => Profiler.loopIncrementerTest(10000), "Loop Incrementer Test", true);
+
+
+
+            Console.WriteLine("\n\n");
         }
     }
 }
